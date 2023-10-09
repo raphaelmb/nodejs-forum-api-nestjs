baeda5b3ce670f588da4175f392717967b6848dc
Raphael Berger <raphaelmb@gmail.com>
2023-10-09 17:00:44 -0300

    add factories + refactor e2e tests

--- test/factories/make-question-comment.ts
+++ test/factories/make-question-comment.ts
@@ -4,6 +4,9 @@ import {
   QuestionCommentProps,
 } from '@/domain/forum/enterprise/entities/question-comment'
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { PrismaQuestionCommentMapper } from '@/infra/database/prisma/mappers/prisma-question-comment-mapper'
 
 export function makeQuestionComment(
   override: Partial<QuestionCommentProps> = {},
@@ -20,4 +23,21 @@ export function makeQuestionComment(
   )
 
   return questionComment
+}
+
+@Injectable()
+export class QuestionCommentFactory {
+  constructor(readonly prisma: PrismaService) {}
+
+  async makePrismaQuestionComment(
+    data: Partial<QuestionCommentProps> = {},
+  ): Promise<QuestionComment> {
+    const questionComment = makeQuestionComment(data)
+
+    await this.prisma.comment.create({
+      data: PrismaQuestionCommentMapper.toPrisma(questionComment),
+    })
+
+    return questionComment
+  }
 }
